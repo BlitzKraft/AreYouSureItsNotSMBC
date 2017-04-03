38362c4fea429172a745653345db216bedb680ce
BK Bolisetty <bk@squarefruitlabs.com>
2017-04-03 02:12:16 -0700

    Basic functionality achieved. Replies to LFC posts
    Also checks if the post has been replied to already. Doesn't double
    comment on the same post.
    
    Added a DEBUG flag.

--- bot.py
+++ bot.py
@@ -3,16 +3,44 @@ import praw
 import os
 import time
 import secrets
+from praw.exceptions import *
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36"
-MESSAGE_TEMPLATE = """Obligatory. \n 
+DEBUG = True
+
+if DEBUG:
+    SUB = "testingground4bots"
+else:
+    SUB = "xkcd"
+
+MESSAGE_TEMPLATE = """\^\^Obligatory. \n 
 
 ***** \n 
 I am a bot. Message me to contact my creator."""
 
-reddit = praw.Reddit(client_id = secrets.CLIENT_ID, client_secret = secrets.CLIENT_SECRET, user_agent = USER_AGENT)
+reddit = praw.Reddit(client_id = secrets.CLIENT_ID,
+             client_secret = secrets.CLIENT_SECRET,
+             user_agent = USER_AGENT,
+             username = secrets.REDDIT_USER,
+             password = secrets.REDDIT_PASSWORD)
 
-for submission in reddit.subreddit("xkcd").new(limit = 20):
+if not os.path.isfile("replied_to.txt"):
+    posts_replied = []
+else:
+    with open("replied_to.txt", "r") as f:
+        posts_replied = f.read()
+        posts_replied = posts_replied.split("\n")
+        posts_replied = list(filter(None, posts_replied))
+
+for submission in reddit.subreddit(SUB).new(limit = 20):
     if "LFC" in submission.title:
-        print("Title: ", submission.title)
-        print("URL: ", submission.url)
+        if submission.id not in posts_replied:
+            submission.reply(MESSAGE_TEMPLATE)
+            if DEBUG:
+                print("Replied to: ", submission.id)
+            posts_replied.append(submission.id)
+            with open("replied_to.txt", "a") as f:
+                f.write(submission.id + "\n")
+        if DEBUG:
+            print("Title: ", submission.title)
+            print("URL: ", submission.url)
